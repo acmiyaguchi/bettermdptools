074ddc268158f061612b54f1744cc9aea485da91
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2023-11-21 00:22:47 -0800

    Use functions instead of lambdas

--- algorithms/rl.py
+++ algorithms/rl.py
@@ -34,7 +34,9 @@ class RL:
         self.render = False
 
     @staticmethod
-    def decay_schedule(init_value, min_value, decay_ratio, max_steps, log_start=-2, log_base=10):
+    def decay_schedule(
+        init_value, min_value, decay_ratio, max_steps, log_start=-2, log_base=10
+    ):
         """
         Parameters
         ----------------------------
@@ -66,148 +68,29 @@ class RL:
         """
         decay_steps = int(max_steps * decay_ratio)
         rem_steps = max_steps - decay_steps
-        values = np.logspace(log_start, 0, decay_steps, base=log_base, endpoint=True)[::-1]
+        values = np.logspace(log_start, 0, decay_steps, base=log_base, endpoint=True)[
+            ::-1
+        ]
         values = (values - values.min()) / (values.max() - values.min())
         values = (init_value - min_value) * values + min_value
-        values = np.pad(values, (0, rem_steps), 'edge')
+        values = np.pad(values, (0, rem_steps), "edge")
         return values
 
     @print_runtime
-    def q_learning(self,
-                   nS=None,
-                   nA=None,
-                   convert_state_obs=lambda state, done: state,
-                   gamma=.99,
-                   init_alpha=0.5,
-                   min_alpha=0.01,
-                   alpha_decay_ratio=0.5,
-                   init_epsilon=1.0,
-                   min_epsilon=0.1,
-                   epsilon_decay_ratio=0.9,
-                   n_episodes=10000):
-        """
-        Parameters
-        ----------------------------
-        nS {int}:
-            Number of states
-
-        nA {int}:
-            Number of available actions
-
-        convert_state_obs {lambda}:
-            The state conversion utilized in BlackJack ToyText problem.
-            Returns three state tuple as one of the 280 converted states.
-
-        gamma {float}, default = 0.99:
-            Discount factor
-
-        init_alpha {float}, default = 0.5:
-            Learning rate
-
-        min_alpha {float}, default = 0.01:
-            Minimum learning rate
-
-        alpha_decay_ratio {float}, default = 0.5:
-            Decay schedule of learing rate for future iterations
-
-        init_epsilon {float}, default = 1.0:
-            Initial epsilon value for epsilon greedy strategy.
-            Chooses max(Q) over available actions with probability 1-epsilon.
-
-        min_epsilon {float}, default = 0.1:
-            Minimum epsilon. Used to balance exploration in later stages.
-
-        epsilon_decay_ratio {float}, default = 0.9:
-            Decay schedule of epsilon for future iterations
-
-        n_episodes {int}, default = 10000:
-            Number of episodes for the agent
-
-
-        Returns
-        ----------------------------
-        Q {numpy array}, shape(nS, nA):
-            Final action-value function Q(s,a)
-
-        pi {lambda}, input state value, output action value:
-            Policy mapping states to actions.
-
-        V {numpy array}, shape(nS):
-            State values array
-
-        Q_track {numpy array}, shape(n_episodes, nS, nA):
-            Log of Q(s,a) for each episode
-
-        pi_track {list}, len(n_episodes):
-            Log of complete policy for each episode
-        """
-        if nS is None:
-            nS=self.env.observation_space.n
-        if nA is None:
-            nA=self.env.action_space.n
-        pi_track = []
-        Q = np.zeros((nS, nA), dtype=np.float64)
-        Q_track = np.zeros((n_episodes, nS, nA), dtype=np.float64)
-        # Explanation of lambda:
-        # def select_action(state, Q, epsilon):
-        #   if np.random.random() > epsilon:
-        #       return np.argmax(Q[state])
-        #   else:
-        #       return np.random.randint(len(Q[state]))
-        select_action = lambda state, Q, epsilon: np.argmax(Q[state]) \
-            if np.random.random() > epsilon \
-            else np.random.randint(len(Q[state]))
-        alphas = RL.decay_schedule(init_alpha,
-                                min_alpha,
-                                alpha_decay_ratio,
-                                n_episodes)
-        epsilons = RL.decay_schedule(init_epsilon,
-                                  min_epsilon,
-                                  epsilon_decay_ratio,
-                                  n_episodes)
-        for e in tqdm(range(n_episodes), leave=False):
-            self.callbacks.on_episode_begin(self)
-            self.callbacks.on_episode(self, episode=e)
-            state, info = self.env.reset()
-            done = False
-            state = convert_state_obs(state, done)
-            while not done:
-                if self.render:
-                    warnings.warn("Occasional render has been deprecated by openAI.  Use test_env.py to render.")
-                action = select_action(state, Q, epsilons[e])
-                next_state, reward, terminated, truncated, _ = self.env.step(action)
-                if truncated:
-                    warnings.warn("Episode was truncated.  Bootstrapping 0 reward.")
-                done = terminated or truncated
-                self.callbacks.on_env_step(self)
-                next_state = convert_state_obs(next_state,done)
-                td_target = reward + gamma * Q[next_state].max() * (not done)
-                td_error = td_target - Q[state][action]
-                Q[state][action] = Q[state][action] + alphas[e] * td_error
-                state = next_state
-            Q_track[e] = Q
-            pi_track.append(np.argmax(Q, axis=1))
-            self.render = False
-            self.callbacks.on_episode_end(self)
-
-        V = np.max(Q, axis=1)
-
-        pi = {s: a for s, a in enumerate(np.argmax(Q, axis=1))}
-        return Q, V, pi, Q_track, pi_track
-
-    @print_runtime
-    def sarsa(self,
-              nS=None,
-              nA=None,
-              convert_state_obs=lambda state, done: state,
-              gamma=.99,
-              init_alpha=0.5,
-              min_alpha=0.01,
-              alpha_decay_ratio=0.5,
-              init_epsilon=1.0,
-              min_epsilon=0.1,
-              epsilon_decay_ratio=0.9,
-              n_episodes=10000):
+    def q_learning(
+        self,
+        nS=None,
+        nA=None,
+        convert_state_obs=lambda state, done: state,
+        gamma=0.99,
+        init_alpha=0.5,
+        min_alpha=0.01,
+        alpha_decay_ratio=0.5,
+        init_epsilon=1.0,
+        min_epsilon=0.1,
+        epsilon_decay_ratio=0.9,
+        n_episodes=10000,
+    ):
         """
         Parameters
         ----------------------------
@@ -271,23 +154,152 @@ class RL:
         pi_track = []
         Q = np.zeros((nS, nA), dtype=np.float64)
         Q_track = np.zeros((n_episodes, nS, nA), dtype=np.float64)
+
         # Explanation of lambda:
         # def select_action(state, Q, epsilon):
         #   if np.random.random() > epsilon:
         #       return np.argmax(Q[state])
         #   else:
         #       return np.random.randint(len(Q[state]))
-        select_action = lambda state, Q, epsilon: np.argmax(Q[state]) \
-            if np.random.random() > epsilon \
-            else np.random.randint(len(Q[state]))
-        alphas = RL.decay_schedule(init_alpha,
-                                min_alpha,
-                                alpha_decay_ratio,
-                                n_episodes)
-        epsilons = RL.decay_schedule(init_epsilon,
-                                  min_epsilon,
-                                  epsilon_decay_ratio,
-                                  n_episodes)
+        def select_action(state, Q, epsilon):
+            return (
+                np.argmax(Q[state])
+                if np.random.random() > epsilon
+                else np.random.randint(len(Q[state]))
+            )
+
+        alphas = RL.decay_schedule(init_alpha, min_alpha, alpha_decay_ratio, n_episodes)
+        epsilons = RL.decay_schedule(
+            init_epsilon, min_epsilon, epsilon_decay_ratio, n_episodes
+        )
+        for e in tqdm(range(n_episodes), leave=False):
+            self.callbacks.on_episode_begin(self)
+            self.callbacks.on_episode(self, episode=e)
+            state, info = self.env.reset()
+            done = False
+            state = convert_state_obs(state, done)
+            while not done:
+                if self.render:
+                    warnings.warn(
+                        "Occasional render has been deprecated by openAI.  Use test_env.py to render."
+                    )
+                action = select_action(state, Q, epsilons[e])
+                next_state, reward, terminated, truncated, _ = self.env.step(action)
+                if truncated:
+                    warnings.warn("Episode was truncated.  Bootstrapping 0 reward.")
+                done = terminated or truncated
+                self.callbacks.on_env_step(self)
+                next_state = convert_state_obs(next_state, done)
+                td_target = reward + gamma * Q[next_state].max() * (not done)
+                td_error = td_target - Q[state][action]
+                Q[state][action] = Q[state][action] + alphas[e] * td_error
+                state = next_state
+            Q_track[e] = Q
+            pi_track.append(np.argmax(Q, axis=1))
+            self.render = False
+            self.callbacks.on_episode_end(self)
+
+        V = np.max(Q, axis=1)
+
+        pi = {s: a for s, a in enumerate(np.argmax(Q, axis=1))}
+        return Q, V, pi, Q_track, pi_track
+
+    @print_runtime
+    def sarsa(
+        self,
+        nS=None,
+        nA=None,
+        convert_state_obs=lambda state, done: state,
+        gamma=0.99,
+        init_alpha=0.5,
+        min_alpha=0.01,
+        alpha_decay_ratio=0.5,
+        init_epsilon=1.0,
+        min_epsilon=0.1,
+        epsilon_decay_ratio=0.9,
+        n_episodes=10000,
+    ):
+        """
+        Parameters
+        ----------------------------
+        nS {int}:
+            Number of states
+
+        nA {int}:
+            Number of available actions
+
+        convert_state_obs {lambda}:
+            The state conversion utilized in BlackJack ToyText problem.
+            Returns three state tuple as one of the 280 converted states.
+
+        gamma {float}, default = 0.99:
+            Discount factor
+
+        init_alpha {float}, default = 0.5:
+            Learning rate
+
+        min_alpha {float}, default = 0.01:
+            Minimum learning rate
+
+        alpha_decay_ratio {float}, default = 0.5:
+            Decay schedule of learing rate for future iterations
+
+        init_epsilon {float}, default = 1.0:
+            Initial epsilon value for epsilon greedy strategy.
+            Chooses max(Q) over available actions with probability 1-epsilon.
+
+        min_epsilon {float}, default = 0.1:
+            Minimum epsilon. Used to balance exploration in later stages.
+
+        epsilon_decay_ratio {float}, default = 0.9:
+            Decay schedule of epsilon for future iterations
+
+        n_episodes {int}, default = 10000:
+            Number of episodes for the agent
+
+
+        Returns
+        ----------------------------
+        Q {numpy array}, shape(nS, nA):
+            Final action-value function Q(s,a)
+
+        pi {lambda}, input state value, output action value:
+            Policy mapping states to actions.
+
+        V {numpy array}, shape(nS):
+            State values array
+
+        Q_track {numpy array}, shape(n_episodes, nS, nA):
+            Log of Q(s,a) for each episode
+
+        pi_track {list}, len(n_episodes):
+            Log of complete policy for each episode
+        """
+        if nS is None:
+            nS = self.env.observation_space.n
+        if nA is None:
+            nA = self.env.action_space.n
+        pi_track = []
+        Q = np.zeros((nS, nA), dtype=np.float64)
+        Q_track = np.zeros((n_episodes, nS, nA), dtype=np.float64)
+
+        # Explanation of lambda:
+        # def select_action(state, Q, epsilon):
+        #   if np.random.random() > epsilon:
+        #       return np.argmax(Q[state])
+        #   else:
+        #       return np.random.randint(len(Q[state]))
+        def select_action(state, Q, epsilon):
+            return (
+                np.argmax(Q[state])
+                if np.random.random() > epsilon
+                else np.random.randint(len(Q[state]))
+            )
+
+        alphas = RL.decay_schedule(init_alpha, min_alpha, alpha_decay_ratio, n_episodes)
+        epsilons = RL.decay_schedule(
+            init_epsilon, min_epsilon, epsilon_decay_ratio, n_episodes
+        )
 
         for e in tqdm(range(n_episodes), leave=False):
             self.callbacks.on_episode_begin(self)
@@ -298,7 +310,9 @@ class RL:
             action = select_action(state, Q, epsilons[e])
             while not done:
                 if self.render:
-                    warnings.warn("Occasional render has been deprecated by openAI.  Use test_env.py to render.")
+                    warnings.warn(
+                        "Occasional render has been deprecated by openAI.  Use test_env.py to render."
+                    )
                 next_state, reward, terminated, truncated, _ = self.env.step(action)
                 if truncated:
                     warnings.warn("Episode was truncated.  Bootstrapping 0 reward.")
